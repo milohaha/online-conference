d777704479f3dfbaceefb3c5cecfce7d6533ea9a
张嘉诚 <milocheung@qq.com>
2021-07-25 16:06:44 +0000

    Complete Essential Dev Env Config

--- README.md
+++ README.md
@@ -8,10 +8,13 @@
 
 已配置好基础Lint和EditorConfig
 
-输入 `docker-compose up -d` 构建并启动服务
+输入 `docker-compose up -d` 构建并启动后端服务
+
+进入`frontend`文件夹输入`npm install`
+
+再输入`npm run serve`成功启动前端服务
 
 正常启动后
 
 - 访问 http://localhost:3000/ 可见ExpressJS的欢迎界面
 - 访问 http://localhost:8080/ 可见VueJS的欢迎界面
-
--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,19 +1,11 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "standard"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-};
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  rules: {}
+}
