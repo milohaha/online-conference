9e2fde65adb2f6382b697608825191580792971b
JiaJu Wang <1600205794@qq.com>
2021-07-26 08:46:44 +0000

    Build frontend homepage

---
+++ frontend/src/views/Home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item active>Home</b-nav-item>
+      <b-nav-item>Link</b-nav-item>
+      <b-nav-item>Another Link</b-nav-item>
+      <b-nav-item disabled>Disabled</b-nav-item>
+    </b-nav>
+
+    <b-jumbotron>
+      <template v-slot:header>富媒体实时协作平台</template>
+
+      <template v-slot:lead>
+        让线上会议室成为可能
+      </template>
+    </b-jumbotron>
+    <div>
+      <b-button pill variant="outline-success" to="/login">登录</b-button>
+    </div>
+    <div>
+      <b-button variant="link" to="/register">点我注册</b-button>
+    </div>
+  </div>
+</template>
---
+++ frontend/src/views/Login/Login.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="login">
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group id="input-group-2" label="UserName:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+      <b-form  @submit.stop.prevent>
+        <label for="feedback-user">User ID</label>
+        <b-input v-model="userId" :state="validation" id="feedback-user"></b-input>
+        <b-form-invalid-feedback :state="validation">
+          Your user ID must be 5-12 characters long.
+        </b-form-invalid-feedback>
+        <b-form-valid-feedback :state="validation">
+          Looks Good.
+        </b-form-valid-feedback>
+      </b-form>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        name: '',
+        food: null,
+        checked: []
+      },
+      foods: [{ text: 'Select One', value: null }, 'Carrots', 'Beans', 'Tomatoes', 'Corn'],
+      show: true,
+      userId: ''
+    }
+  },
+  computed: {
+    validation () {
+      return this.userId.length > 4 && this.userId.length < 13
+    }
+  },
+  methods: {
+    onSubmit (evt) {
+      evt.preventDefault()
+      alert(JSON.stringify(this.form))
+    },
+    onReset (evt) {
+      evt.preventDefault()
+      // Reset our form values
+      this.form.email = ''
+      this.form.name = ''
+      this.form.food = null
+      this.form.checked = []
+      // Trick to reset/clear native browser form validation state
+      this.show = false
+      this.$nextTick(() => {
+        this.show = true
+      })
+    }
+  }
+}
+</script>
+<style>
+#login{
+  width: 400px;
+  height: 700px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-left: -200px;
+  margin-top: -350px;
+}
+</style>
